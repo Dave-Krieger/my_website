aef9f2219f16f1e6b5bbc01fbbb7e435cb2e7c52
dave-krieger <davidkrieger95@gmail.com>
2019-07-21 13:36:02 -0700

    Improved tech_dets webpage format, disabled navbar URLS

--- about_me/urls.py
+++ about_me/urls.py
@@ -6,9 +6,8 @@ app_name = 'about_me'
 urlpatterns = [
     path('about_me_index',views.about_me_index,name='about_me_index'),
     path('hobbies',views.hobbies,name='hobbies'),
-    path('class_history',views.class_history,name='class_history'),
-    path('comp_sci',views.comp_sci,name='comp_sci'),
-    path('cog_sci',views.cog_sci,name='cog_sci'),
+    path('leader',views.leader,name='leader'),
+    path('programming',views.programming,name='programming'),
     path('tech_dets',views.tech_dets,name='tech_dets'),
 
 ]
--- about_me/views.py
+++ about_me/views.py
@@ -3,17 +3,14 @@ from django.shortcuts import render
 def about_me_index(request):
     return render(request, 'about_me/index.html')
 
-def comp_sci(request):
-    return render(request, 'about_me/comp_sci.html')
-
-def cog_sci(request):
-    return render(request, 'about_me/cog_sci.html')
-
-def tech_dets(request):
-    return render(request, 'about_me/tech_dets.html')
-
 def hobbies(request):
     return render(request, 'about_me/hobbies.html')
 
-def class_history(request):
-    return render(request, 'about_me/class_history.html')
+def programming(request):
+    return render(request, 'about_me/programming.html')
+
+def leader(request):
+    return render(request, 'about_me/leader.html')
+
+def tech_dets(request):
+    return render(request, 'about_me/tech_dets.html')
