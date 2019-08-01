87d6e3aa0ee756b69bec698b063f6e6b6d626584
Dave-Krieger <davidkrieger95@gmail.com>
2019-08-01 16:59:38 -0700

    Overhauling website with 3rd party template

--- main_page/views.py
+++ main_page/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+import subprocess
+
+
 
 # Create your views here.
 def index(request):
+    r = subprocess.call("git log -1 --date=short --pretty=format:%cd", shell=True)
+    print(r)
     return render(request, 'main_page/index.html')
